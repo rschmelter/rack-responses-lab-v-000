d73b0d6b7c5f2084b6600e577040e2314ce19606
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-18 18:12:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,10 @@ class Application
     resp = Rack::Response.new
     date_time = Time.now
     time = date_time.hour
+    if time < 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
     resp.finish
   end
 
